b62be59f11f6cc3c7444818d0c1a3eef20f389d1
Alexandrov <Djekkson12@gmail.com>
2019-12-09 12:10:55 +0200

    SecondCommit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,7 +15,6 @@ public class Main {
         RugbyTeam rugbyTeam4 = new RugbyTeam("Ireland Team");
 
 
-
         FootballTeam footballTeam1 = new FootballTeam("England Team");
         FootballTeam footballTeam2 = new FootballTeam("Germany Team");
         FootballTeam footballTeam3 = new FootballTeam("Brazil Team");
